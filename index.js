d6d2643e2e061bb3cadf3d0aed9d3b99b4deb3b8
Lennert Van sever <lennert@lennerts-mbp.home>
2017-12-14 15:35:36 +0100

    added first test reply function

--- index.js
+++ index.js
@@ -47,3 +47,22 @@ app.post('/webhook/', function (req, res) {
     }
     res.sendStatus(200)
 })
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:facebookPageToken},
+	    method: 'POST',
+		json: {
+		    recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+		    console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
+    })
+}
