e1392e45dda614e4c55e6c99d32b30193061de38
Lennert Van sever <lennert@lennerts-mbp.home>
2017-12-14 16:46:04 +0100

    checking count of messages is not null

--- index.js
+++ index.js
@@ -37,11 +37,13 @@ app.listen(app.get('port'), function() {
 
 
 app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-      let event = req.body.entry[0].messaging[i]
-      let sender = event.sender.id;
-      chatLogic.main(event, sender);
+    let messaging_events = req.body.entry[0].messaging;
+    if(messaging_events > 0){
+	    for (let i = 0; i < messaging_events.length; i++) {
+	      let event = req.body.entry[0].messaging[i]
+	      let sender = event.sender.id;
+	      chatLogic.main(event, sender);
+	    }
     }
     res.sendStatus(200)
 })
