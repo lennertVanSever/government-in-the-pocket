7cea22202ed9775e4c0dc6e5bf6afbc5e184fcd0
Lennert Van sever <lennert@lennerts-mbp.home>
2017-12-14 16:48:18 +0100

    removed checking if null

--- index.js
+++ index.js
@@ -38,13 +38,13 @@ app.listen(app.get('port'), function() {
 
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging;
-    if(messaging_events > 0){
+    
 	    for (let i = 0; i < messaging_events.length; i++) {
 	      let event = req.body.entry[0].messaging[i]
 	      let sender = event.sender.id;
 	      chatLogic.main(event, sender);
 	    }
-    }
+ 
     res.sendStatus(200)
 })
 
